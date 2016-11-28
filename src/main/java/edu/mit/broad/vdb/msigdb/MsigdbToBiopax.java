3af03e2b21c8c58c9bb823d231691810ab8ab743
IgorRodchenkov <rodche@gmail.com>
2016-11-28 18:39:42 -0500

    removed log4j and some debug logs...

--- src/main/java/edu/mit/broad/vdb/msigdb/MsigdbToBiopax.java
+++ src/main/java/edu/mit/broad/vdb/msigdb/MsigdbToBiopax.java
@@ -3,13 +3,10 @@ package edu.mit.broad.vdb.msigdb;
 import edu.mit.broad.vdb.msigdb.converter.MsigdbToBiopaxConverter;
 import org.biopax.paxtools.io.SimpleIOHandler;
 import org.biopax.paxtools.model.Model;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.FileOutputStream;
 
 public class MsigdbToBiopax {
-    private static Logger log = LoggerFactory.getLogger(MsigdbToBiopax.class);
 
     public static void main(String[] args) throws Exception {
         if(args.length < 2) {
@@ -21,17 +18,14 @@ public class MsigdbToBiopax {
         }
 
         String msigdbFile = args[0].trim();
-        log.info("MSigDB File: " + msigdbFile);
         MsigdbToBiopaxConverter converter = new MsigdbToBiopaxConverter();
         Model model = converter.convert(msigdbFile);
-
         SimpleIOHandler simpleIOHandler = new SimpleIOHandler();
         String outputFile = args[1].trim();
-        log.info("Conversion done. Now exporting the BioPAX model as a file: " + outputFile);
         FileOutputStream outputStream = new FileOutputStream(outputFile);
         simpleIOHandler.convertToOWL(model, outputStream);
         outputStream.close();
-
-        log.info("All done.");
+        System.out.println("All done.");
     }
+
 }
